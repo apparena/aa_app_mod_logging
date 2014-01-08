74c6004901712e1e794e9d87590b610c3743e280
Marcus Merchel <kontakt@marcusmerchel.de>
2014-01-08 17:31:12 +0100

    GNR-6 added demolink configs

---
+++ configs/demolinks.php
@@ -0,0 +1,3 @@
+<a href="#page/logging/demo/action" class="btn btn-default">Action-Log Demo</a>
+<a href="#page/logging/demo/admin" class="btn btn-default">Admin-Log Demo</a>
+<a href="#page/logging/demo/group" class="btn btn-default">Group-Log Demo</a>
